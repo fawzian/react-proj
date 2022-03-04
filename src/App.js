8e4dd84ec0bdb29294e809aef7a0e46a3e8a0fc0
FAWZIA NUR <FAWZIANUR@GMAIL.com>
2022-03-03 20:07:35 -0500

    sorting out errors - with components and functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+// import React from "react";
 
-function App() {
+// import {Header} from "./src/components/Header"
+// import {Home} from "./Home"
+// import {List} from "./List"
+
+// import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+
+
+// function App() {
+//   return (<Router>
+//       <Header/>
+//   </Router>
+
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+import {Header} from "./components/Header.js"
+import {Home} from "./components/Home"
+import {List} from "./components/List"
+
+
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+export const App = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>whatever I want
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    <Header/>
+  
+  )
+};
---
+++ test.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export const Test = (props) => {
+    return <h1>Test</h1>
+}
